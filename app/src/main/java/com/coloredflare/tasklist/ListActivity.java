3e789b354039ae1d83ce12e8fbd9e20bc4ad3e90
MihaelaNicoleta <raumiha21@gmail.com>
2015-03-21 13:15:13 +0200

    TaskAdapter implemented + ListActivity + intents

--- app/src/main/java/com/coloredflare/tasklist/ListActivity.java
+++ app/src/main/java/com/coloredflare/tasklist/ListActivity.java
@@ -1,9 +1,7 @@
 package com.coloredflare.tasklist;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,23 +11,27 @@ import android.widget.ListView;
 import com.coloredflare.tasklist.db.Database;
 import com.coloredflare.tasklist.db.DatabaseFactory;
 import com.coloredflare.tasklist.db.Lists;
+import com.coloredflare.tasklist.db.Task;
+import com.coloredflare.tasklist.db.Tasks;
 
 
-public class Main extends ActionBarActivity {
+public class ListActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_list);
+
+        int listId = getIntent().getExtras().getInt("listId");
 
         ListView listView = (ListView) findViewById(R.id.listView);
 
 
-		Database database = DatabaseFactory.getDatabase();
-		final Lists values = database.getLists();
+        Database database = DatabaseFactory.getDatabase();
+        final Tasks values = database.getTasks(listId);
 
 
-        ListAdapter adapter = new ListAdapter(this, R.layout.list_simple_textview, values);
+        TaskAdapter adapter = new TaskAdapter(this, R.layout.list_simple_textview, values);
 
         listView.setAdapter(adapter);
 
@@ -37,15 +39,14 @@ public class Main extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Intent intent = new Intent(Main.this, ListActivity.class);
-                intent.putExtra("listId", id);
-                startActivity(intent);
+
 
             }
         };
 
         listView.setOnItemClickListener(listener);
-
     }
+
+
 
 }
---
+++ app/src/main/java/com/coloredflare/tasklist/TaskAdapter.java
@@ -0,0 +1,53 @@
+package com.coloredflare.tasklist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.coloredflare.tasklist.db.Tasks;
+
+public class TaskAdapter extends BaseAdapter {
+
+    private Context context;
+    private int resource;
+    private Tasks tasks;
+
+    public TaskAdapter(Context context, int resource, Tasks tasks) {
+        this.context = context;
+        this.resource = resource;
+        this.tasks = tasks;
+
+    }
+    @Override
+    public int getCount() {
+        return tasks.count();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return tasks.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return tasks.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rootView;
+        if(convertView == null)
+            rootView = inflater.inflate(resource, parent, false);
+        else
+            rootView = convertView;
+
+        TextView textView = (TextView) rootView.findViewById(R.id.textview);
+        textView.setText(tasks.get(position).toString());
+
+        return rootView;
+    }
+}
