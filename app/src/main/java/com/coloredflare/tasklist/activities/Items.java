7b5b906797ba00e666bebbd7e7fcc121496fb7f1
Eli <coloredflare@gmail.com>
2015-06-04 16:40:07 +0300

    get token+ example json

--- app/src/main/java/com/coloredflare/tasklist/activities/Items.java
+++ app/src/main/java/com/coloredflare/tasklist/activities/Items.java
@@ -7,10 +7,23 @@ import com.coloredflare.tasklist.R;
 
 public class Items extends ActionBarActivity {
 
+	private String token;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		token = getToken();
+
 		setContentView(R.layout.items);
+	}
+
+	private String getToken() {
+		return getIntent().getStringExtra("token");
+	}
+
+	private String getItems() {
+		return getString(R.string.test_items);
 	}
 
 }
