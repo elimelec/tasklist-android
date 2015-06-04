6980280893ffff9e4651b1eecfccad3dd60c6bd8
Eli <coloredflare@gmail.com>
2015-06-04 18:17:52 +0300

    type

--- app/src/main/java/com/coloredflare/tasklist/datatypes/Item.java
+++ app/src/main/java/com/coloredflare/tasklist/datatypes/Item.java
@@ -6,12 +6,14 @@ import org.json.JSONObject;
 public class Item {
 	public String name = "";
 	public int id = 0;
+	public String type = "";
 
 	public static Item fromJSON(JSONObject json) {
 		Item item = new Item();
 		try {
 			item.name = json.getString("name");
 			item.id = json.getInt("id");
+			item.type = json.getString("type");
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
